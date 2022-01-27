c02c063a70b85555144804a7a30d276be10c641c
Pratiksurya1 <suryawanshipratik356@gmail.com>
2022-01-27 07:56:05 +0530

    Add project files.

---
+++ PatternCheck.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Regex_Demo
+{
+  
+    internal class PatternCheck
+    {
+      //  public string Regex_Pattern = "^[A-Za-z]{3,}$";
+
+        public bool FirstName(string FirstName)
+        {
+             string Regex_Pattern = "^[A-Za-z]{3,}$";
+
+            return Regex.IsMatch(FirstName, Regex_Pattern);
+        }
+    }
+}
